aff2384f4b8ebd9c251b14e8b2be992e47bd9ddf
Suresh Pendap <spendap@walmartlabs.com>
2018-01-04 18:07:21 -0800

    changed the property values to empty strings

--- clouds/solr-service.rb
+++ clouds/solr-service.rb
@@ -12,18 +12,18 @@ service 'solr-service',
         :provides => {:service => 'solr-service'},
         :attributes => {
             :solr_custom_params => '{
-              "block_expensive_queries_class": "com.walmart.strati.search.solr.BlockExpensiveQueries",
-              "slow_query_logger_class": "com.walmart.strati.search.solr.SlowQueryLogger",
-              "query_source_tracker_class": "com.walmart.strati.search.solr.QuerySourceTracker",
-              "solr_dosfilter_class": "com.walmart.strati.search.solr.CustomDoSFilter",
-              "df_graphite_servers": "[\"graphite-solrcloud.solr-graphite-cdc.graphite-solrcloud-monitor.ms-df-solrcloud.prod.walmart.com:2003\",\"graphite-solrcloud.solr-graphite-ndc.graphite-solrcloud-monitor.ms-df-solrcloud.prod.walmart.com:2003\"]",
-              "release_urlbase": "http://nexus.prod.walmart.com/nexus/content/repositories/pangaea_releases/",
-              "snapshot_urlbase": "http://gec-maven-nexus.walmart.com/nexus/content/repositories/pangaea_snapshots/",
-              "solr_custom_comp_artifact": "com.walmart.strati.search.solr:solr-custom-components",
-              "slow_query_logger": "log4j.logger.com.walmart.search.solr.SlowQueryLogger.QueryLog",
-              "jetty_filter_url": "http://nexus.prod.walmart.com/nexus/content/repositories/pangaea_releases/com/walmart/strati/search/solr/solr-jetty-servlets/0.0.1/solr-jetty-servlets-0.0.1.jar",
-              "config_url_v5": "http://nexus.prod.walmart.com/nexus/content/repositories/pangaea_releases/com/walmart/platform/forklift/solrpack/default-forklift-solr-5.5.1/1.0.1/default-forklift-solr-5.5.1-1.0.1.jar",
-              "config_url_v6": "http://nexus.prod.walmart.com/nexus/content/repositories/pangaea_releases/com/walmart/platform/forklift/solrpack/default-forklift-solr-6.3.0/1.0.4/default-forklift-solr-6.3.0-1.0.4.jar",
-              "config_url_v7": "http://gec-maven-nexus.walmart.com/content/repositories/pangaea_releases/com/walmart/platform/forklift/solrpack/default-forklift-solr-7.1.0/1.0.5/default-forklift-solr-7.1.0-1.0.5.jar"
+              "block_expensive_queries_class": "",
+              "slow_query_logger_class": "",
+              "query_source_tracker_class": "",
+              "solr_dosfilter_class": "",
+              "df_graphite_servers": "[]",
+              "release_urlbase": "",
+              "snapshot_urlbase": "",
+              "solr_custom_comp_artifact": "",
+              "slow_query_logger": "",
+              "jetty_filter_url": "",
+              "config_url_v5": "",
+              "config_url_v6": "",
+              "config_url_v7": ""
             }'
         }
